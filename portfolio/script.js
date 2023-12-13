a11289f993e9fe099ddbf081d261f99d5b709170
yusuf123-hub <114059369+yusuf123-hub@users.noreply.github.com>
2023-12-14 04:50:55 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,8 @@
+// Header Toggle
+
+let MenuBtn = document.getElementById('MenuBtn')
+
+MenuBtn.addEventListener('click' ,function(e){
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+})
